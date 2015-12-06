9c341f0fc96fc0dea73e7771fe979ab75b7df4a5
Maciej Górski <maciek.gorski@gmail.com>
2015-12-06 15:31:45 +0100

    Logging in using retinal scan.

--- app/src/main/java/pl/droidcon/dangerousboa/LoginProgressActivity.kt
+++ app/src/main/java/pl/droidcon/dangerousboa/LoginProgressActivity.kt
@@ -1,9 +1,9 @@
 package pl.droidcon.dangerousboa
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
-import io.relayr.android.RelayrSdk
 import io.relayr.java.model.TransmitterDevice
 
 class LoginProgressActivity : AppCompatActivity() {
@@ -11,11 +11,16 @@ class LoginProgressActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         RelayrSdkInitializer.init(this)
-        TransmitterDevice("092fda27-a675-463e-8197-654436a65a3f", "f.x-UR.1Gmb-Kooo98hgZT5i6yp5e4QH", "d3d7d57b-dd5c-408f-a831-cc4ad2cf4fc5", "light", "a7ec1b21-8582-4304-b1cf-15a1fc66d1e8").subscribeToCloudReadings().subscribe({
-            val value = it.value
-            Log.e("tag", "value: " + value)
-        }, {
-            Log.e("tag", "error", it)
-        })
+        TransmitterDevice("9e5d9ac5-96fc-459a-a3d2-8946b6e2c961", "f.x-UR.1Gmb-Kooo98hgZT5i6yp5e4QH", "d3d7d57b-dd5c-408f-a831-cc4ad2cf4fc5", "light", "a7ec1b21-8582-4304-b1cf-15a1fc66d1e8").subscribeToCloudReadings()
+                .filter { it.meaning == "proximity" }
+                .map { it.value as Double }
+                .filter { it > 200 }
+                .take(1)
+                .subscribe({
+                    Log.e("tag", "value: " + it)
+                    startActivity(Intent(this, MusicActivity::class.java))
+                }, {
+                    Log.e("tag", "error", it)
+                })
     }
 }
