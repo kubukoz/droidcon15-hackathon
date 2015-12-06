8a3f66cd4b35e1f6c7226bed095116137c501c32
Jakub Kozłowski <kubukoz@gmail.com>
2015-12-06 16:40:11 +0100

    Song is selected from firebase

--- app/src/main/java/pl/droidcon/dangerousboa/MusicActivity.kt
+++ app/src/main/java/pl/droidcon/dangerousboa/MusicActivity.kt
@@ -19,30 +19,23 @@ class MusicActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         (getSystemService(VIBRATOR_SERVICE) as Vibrator).vibrate(200)
-        val prefs: List<SoundPrefs> = listOf(
-                SoundPrefs(true, true, true, true),
-                SoundPrefs(true, false, false, true),
-                SoundPrefs(false, true, true, true))
 
         if ((getSystemService(USER_SERVICE) as UserManager).isUserAGoat)
             Toast.makeText(this, "You are a goat!", Toast.LENGTH_SHORT).show()
 
-        selectedSong = getSongFromPrefs(prefs)
-
-        player.setVolume(1f, 1f)
-        player.start()
-
         val firebase = Firebase("https://incandescent-inferno-5098.firebaseio.com/users")
-        firebase.addValueEventListener(object: ValueEventListener {
+        firebase.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(data: DataSnapshot) {
-                val allUsersPrefs = data.children.map {
+                selectedSong = getSongFromPrefs(data.children.map {
                     val list = it.value as List<Boolean>
                     SoundPrefs(list[0], list[1], list[2], list[3])
-                }
+                })
+
+                player.setVolume(1f, 1f)
+                player.start()
             }
 
-            override fun onCancelled(error: FirebaseError) {
-            }
+            override fun onCancelled(error: FirebaseError) { }
         })
         firebase.updateChildren(mapOf(userId to soundPrefs().asList()))
     }
