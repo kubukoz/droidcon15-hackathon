9655c31eaa6a9987bd9f7115bdd6cfcd113c0814
Jakub Kozłowski <kubukoz@gmail.com>
2015-12-06 17:07:34 +0100

    Even longer splash!

--- app/src/main/java/pl/droidcon/dangerousboa/SplashActivity.kt
+++ app/src/main/java/pl/droidcon/dangerousboa/SplashActivity.kt
@@ -12,6 +12,6 @@ class SplashActivity : AppCompatActivity() {
         Handler().postDelayed({
             startActivity(Intent(this, LoginActivity::class.java))
             finish()
-        }, 2000)
+        }, 4000)
     }
 }
