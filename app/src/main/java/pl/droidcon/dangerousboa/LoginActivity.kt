3b291f29b0ae6fd2c7d9fa37fe9b9d519a3d4bd5
Jakub Kozłowski <kubukoz@gmail.com>
2015-12-06 14:44:34 +0100

    Change pref button to menu button

--- app/src/main/java/pl/droidcon/dangerousboa/LoginActivity.kt
+++ app/src/main/java/pl/droidcon/dangerousboa/LoginActivity.kt
@@ -3,6 +3,9 @@ package pl.droidcon.dangerousboa
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 
 class LoginActivity : AppCompatActivity() {
 
@@ -13,8 +16,20 @@ class LoginActivity : AppCompatActivity() {
         findViewById(R.id.login_login).setOnClickListener {
             startActivity(Intent(this, LoginProgressActivity::class.java))
         }
-        findViewById(R.id.login_prefs).setOnClickListener {
-            startActivity(Intent(this, SoundPreferencesActivity::class.java))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when (item?.itemId) {
+            R.id.action_settings -> {
+                startActivity(Intent(this, SoundPreferencesActivity::class.java))
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
