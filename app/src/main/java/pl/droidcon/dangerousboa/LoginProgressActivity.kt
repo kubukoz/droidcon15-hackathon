a169a36fb79b00ceb35a1c101901c2ec47f8e27d
Jakub Kozłowski <kubukoz@gmail.com>
2015-12-06 16:21:18 +0100

    Some stuff

--- app/src/main/java/pl/droidcon/dangerousboa/LoginProgressActivity.kt
+++ app/src/main/java/pl/droidcon/dangerousboa/LoginProgressActivity.kt
@@ -4,23 +4,36 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
-import io.relayr.java.model.TransmitterDevice
+import io.relayr.android.RelayrSdk
+import rx.android.schedulers.AndroidSchedulers
 
 class LoginProgressActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_progress)
         RelayrSdkInitializer.init(this)
-        TransmitterDevice("9e5d9ac5-96fc-459a-a3d2-8946b6e2c961", "f.x-UR.1Gmb-Kooo98hgZT5i6yp5e4QH", "d3d7d57b-dd5c-408f-a831-cc4ad2cf4fc5", "light", "a7ec1b21-8582-4304-b1cf-15a1fc66d1e8").subscribeToCloudReadings()
-                .filter { it.meaning == "proximity" }
-                .map { it.value as Double }
-                .filter { it > 200 }
-                .take(1)
-                .subscribe({
-                    Log.e("tag", "value: " + it)
-                    startActivity(Intent(this, MusicActivity::class.java))
-                }, {
-                    Log.e("tag", "error", it)
-                })
+        if (!RelayrSdk.isUserLoggedIn())
+            RelayrSdk.logIn(this)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (RelayrSdk.isUserLoggedIn())
+            RelayrSdk.getUser().observeOn(AndroidSchedulers.mainThread()).subscribe {
+                RelayrSdk.getUserApi().getDevices(it.id).subscribe { devices ->
+                    devices.forEach {
+                        it.subscribeToCloudReadings()
+                                .map { it.value as Double }
+                                //                            .filter { it > 1 }
+                                .subscribe({
+                                    Log.e("tag", "value: " + it)
+                                    startActivity(Intent(this, MusicActivity::class.java))
+                                }, {
+                                    Log.e("tag", "error", it)
+                                })
+                    }
+                }
+            }
     }
 }
--- app/src/main/java/pl/droidcon/dangerousboa/MusicActivity.kt
+++ app/src/main/java/pl/droidcon/dangerousboa/MusicActivity.kt
@@ -3,6 +3,7 @@ package pl.droidcon.dangerousboa
 import android.media.MediaPlayer
 import android.os.Bundle
 import android.os.UserManager
+import android.os.Vibrator
 import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
 
@@ -13,14 +14,29 @@ class MusicActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        (getSystemService(VIBRATOR_SERVICE) as Vibrator).vibrate(200)
+        val prefs: List<SoundPrefs> = listOf(
+                SoundPrefs(true, true, true, true),
+                SoundPrefs(true, false, false, true),
+                SoundPrefs(false, true, true, true))
 
         if ((getSystemService(USER_SERVICE) as UserManager).isUserAGoat)
             Toast.makeText(this, "You are a goat!", Toast.LENGTH_SHORT).show()
 
-        selectedSong = 0
+        selectedSong = getSongFromPrefs(prefs)
 
         player.setVolume(1f, 1f)
         player.start()
+    }
+
+    private fun getSongFromPrefs(prefs: List<SoundPrefs>): Int {
+        val classicals = prefs.sumBy { if (it.classical) 1 else 0 }
+        val electros = prefs.sumBy { if (it.electro) 1 else 0 }
+        val raps = prefs.sumBy { if (it.rap) 1 else 0 }
+        val rocks = prefs.sumBy { if (it.rock) 1 else 0 }
+
+        val results = listOf(classicals, electros, raps, rocks)
+        return (results zip results.indices).maxBy { it.first }!!.second
     }
 
     fun getSong() = when (selectedSong) {
